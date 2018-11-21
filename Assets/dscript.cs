c70e4d9437d162c652506d413f622a3f2b81d3a2
usman420 <usman.aslam19392@gmail.com>
2018-11-21 15:19:37 +0100

    game

---
+++ Assets/dscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dscript : MonoBehaviour
+{
+    private Animator _animator = null;
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        _animator.SetBool("isopen", true);
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        _animator.SetBool("isopen", false);
+    }
+}
---
+++ Assets/falshlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falshlight : MonoBehaviour
+
+    {
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
+
